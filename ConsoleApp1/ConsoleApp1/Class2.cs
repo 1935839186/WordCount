11eac3798354c167883881df0b5033d6dda7b493
小小东 <1935839186@qq.com>
2019-04-04 17:44:13 +0800

    完成单词个数统计

---
+++ ConsoleApp1/ConsoleApp1/Class2.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+//using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    class Count
+    {
+        public void Countzifu(string All)//统计字符数
+        {
+            string A = All;
+            Console.WriteLine("文件中的字符数是：{0}",A.Length);
+        }
+        public void Countline()//统计有效行
+        {
+            int i = 0;
+            Console.Write("请输入想要统计行的文件:");
+            string fx = Console.ReadLine();
+            if (File.Exists(fx))
+            {
+                string fn = @fx;
+               
+                StreamReader fs = new StreamReader(fn);
+                string line;
+                while ((line = fs.ReadLine()) != null)
+                {
+                    i++;
+                }
+            }
+            Console.WriteLine("文件的有效行数为：{0}",i);
+
+        }
+        public void Countword(string All)//统计单词数
+        {
+            //string str = All.Replace("\n","").Replace("\r","");
+            string str = All;
+            string[] sword = str.Split(new char[4] { ',', ' ','.' ,'\n'});
+            int i ,n;
+            int j = 0;
+            int m = 0;
+            for (i = 0; i < sword.Length; i++)
+            {
+                char[] cc = sword[i].ToCharArray();
+                for (n=0;n<cc.Length;n++)
+                {
+                    //Console.WriteLine(cc[n]);
+                   if ((cc[n] >= 65 && cc[n] <= 90) || (cc[n] >= 97 && cc[n] <= 122))
+                    {
+                        m++;
+                   }
+                    if (sword[i].Length >= 4&&m==4)
+                    {
+                        Console.WriteLine(sword[i]);
+                        j++;
+                   }
+                }
+            }
+                
+           Console.WriteLine("单词个数为：{0}",j);
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string All;
+            WordCount A = new WordCount();
+            All=A.Read();
+           // Console.WriteLine(All);
+            Count B = new Count();
+            B.Countzifu(All);
+            Console.WriteLine("-----");
+           // B.Countline();
+           // Console.WriteLine("-----");
+            B.Countword(All);
+        }
+    }
+}
